377ed5741e713e26dfd63f16ccf7635826dfd27f
divyanshika <guptadivyanshika16@gmail.com>
2026-02-05 11:15:39 +0530

    server

---
+++ 5 feb/index.js
@@ -0,0 +1,26 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
+
+const _filename=fileURLToPath(import.meta.url);
+const _dirname=path.dirname(_filename);
+
+const server=http.createServer((req,res)=>{
+  const filePath=path.join(_dirname,"message.txt");
+
+  fs.readFile(filePath,"utf8",(err,data)=>{
+    if(err){
+      res.writeHead(500,{"Content-Type":"text/plain"});
+      res.end("error reading file");
+    }
+    else{
+      res.writeHead(200,{"Content-Type":"text/plain"});
+      res.end(data);
+    };
+  })
+})
+
+server.listen(3000,()=>{
+  console.log("Server is running on port 3000");
+});
